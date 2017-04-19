36cf00b34f5e7dbc41eb00d6480ec4d9e6a949de
NaMach <naren.wins@gmail.com>
2017-04-19 18:16:55 -0400

    added files directory to the module and added custom index.html page

--- Modules/webserver/manifests/homepage.pp
+++ Modules/webserver/manifests/homepage.pp
@@ -2,14 +2,6 @@ class webserver::homepage inherits webserver::params {
     file { '/var/www/html/index.html' :
         ensure => file,
         #source => 'puppet:///modules/class/file.txt';
-        content => file('apache/index.html'),
+        content => file('webserver/index.html'),
     }
-
-    service { $webserver::params::http_service:
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
-    }
-
 }
